f558e2122237f5088cca7f9398e51ace21d0c971
mikoto2000 <mikoto2000@gmail.com>
2020-10-04 10:25:19 +0900

    Update debian directory for athrill.

--- debian/athrill-rh850f1x.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the athrill-rh850f1x package
-#
-0 4	* * *	root	[ -x /usr/bin/athrill-rh850f1x_maintenance ] && /usr/bin/athrill-rh850f1x_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(athrill-rh850f1x):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="athrill-rh850f1x" command="/usr/bin/athrill-rh850f1x"
